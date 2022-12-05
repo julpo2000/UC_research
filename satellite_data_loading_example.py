d436ded0a56d06ef6dba54a38cdefd3d2d4e2a7b
Phobos <j-elmer-prins@live.nl>
2022-12-05 18:05:12 +0100

    pandas loading and plotting

--- satellite_data_loading_example.py
+++ satellite_data_loading_example.py
@@ -4,46 +4,67 @@ import matplotlib.pyplot as plt
 import pydap
 import getpass
 import xarray as xr
+import rioxarray as rxr
 import netCDF4
 import numpy as np
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+import seaborn as sns
+
 file_name1 = "data/S5P_PAL__L2__NO2____20210101T031206_20210101T045336_16681_01_020301_20211112T015828.nc"
 file_name2 = "S5P_PAL__L2__NO2____20211101T100047_20211101T114216_20998_02_020301_20211215T122137.nc"
 file_name3 = "S5P_PAL__L2__NO2____20210825T011458_20210825T025628_20028_02_020301_20211108T083237.nc"
 
 fire_data_name = "data/S5P_PAL__L2__NO2____20210101T150236_20210101T164406_16688_01_020301_20211112T020118.nc"
-# data = xr.open_dataset(file_name2, engine="netcdf4")
-#
-#
-# print(data.info)
-#
-# print(data.summary)
-# #
-# # print(data.info)
-#
-# print(data.coords)
-#
-# print(data.attrs)
-# print(data['PRODUCT'])
+fire_data2_name = "data/S5P_PAL__L2__NO2____20210104T172836_20210104T191006_16732_01_020301_20211112T021911.nc"
+fire_data3_name = "data/S5P_PAL__L2__NO2____20210101T164406_20210101T182536_16689_01_020301_20211112T020123.nc"
 
 
-file = netCDF4.Dataset(fire_data_name, format="NETCDF4")
-# print(file.groups)
-# print(file['PRODUCT/nitrogendioxide_tropospheric_column'])
+file = netCDF4.Dataset(fire_data3_name, format="NETCDF4")
 NO_data = file['PRODUCT/nitrogendioxide_tropospheric_column']
 lat_data = np.array(file['PRODUCT/latitude'])
 long_data = np.array(file['PRODUCT/longitude'])
 
 NO_data = np.array(NO_data)
-# print(lat_data)
-# # print(NO_data)
-# print(lat_data)
-print(long_data)
-NO_data = NO_data.reshape((4173, 450))
 
+print(long_data)
+print(long_data.shape)
+NO_data = NO_data.reshape(long_data.shape).squeeze()
 
 plt.imshow(NO_data)
 
-# plt.show()
+plt.show()
+
+amount_of_data_points = -1
+remove_masked_values = True
+partial_plot = True
+longitude_range = (-85, -75)
+latitude_range = (20, 35)
+
+
+data = np.array([NO_data.flatten()[:amount_of_data_points], long_data.flatten()[:amount_of_data_points], lat_data.flatten()[:amount_of_data_points]]).transpose()
+columns = ["NO", "Longitude", "Latitude"]
+data_frame = pd.DataFrame(data=data, columns=columns)
+
+print(data_frame.head())
+geometry = [Point(xy) for xy in zip(data_frame["Longitude"], data_frame["Latitude"])]
+geo_dataframe = gpd.GeoDataFrame(data_frame, crs={'init': "epsg:4326"}, geometry=geometry)
+
+if remove_masked_values:
+    print("removing masked values..")
+    geo_dataframe = geo_dataframe[geo_dataframe["NO"] < 1]
+if partial_plot:
+    print("removing values outside long/lat range..")
+    geo_dataframe = geo_dataframe[geo_dataframe["Longitude"] > longitude_range[0]]
+    geo_dataframe = geo_dataframe[geo_dataframe["Longitude"] < longitude_range[1]]
+    geo_dataframe = geo_dataframe[geo_dataframe["Latitude"] > latitude_range[0]]
+    geo_dataframe = geo_dataframe[geo_dataframe["Latitude"] < latitude_range[1]]
+
+print(geo_dataframe.head())
+
+fig, ax = plt.subplots(1, 1)
+geo_dataframe.plot(column="NO", ax=ax, legend=True)
+plt.show()
 
 
 # gebruiken voor data
