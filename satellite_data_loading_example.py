e552ccf74a623ff40c4487495f1c24b3751c33ca
Phobos <j-elmer-prins@live.nl>
2022-12-05 18:44:52 +0100

    changed order of filtering

--- satellite_data_loading_example.py
+++ satellite_data_loading_example.py
@@ -47,24 +47,23 @@ data = np.array([NO_data.flatten()[:amount_of_data_points], long_data.flatten()[
 columns = ["NO", "Longitude", "Latitude", "qa"]
 data_frame = pd.DataFrame(data=data, columns=columns)
 
+if filter_quality:
+    print("filtering on quality..")
+    data_frame = data_frame[data_frame["qa"] >= minimal_data_quality]
+if remove_masked_values:
+    print("removing masked values..")
+    data_frame = data_frame[data_frame["NO"] < 1]
+if partial_plot:
+    print("removing values outside long/lat range..")
+    data_frame = data_frame[data_frame["Longitude"] > longitude_range[0]]
+    data_frame = data_frame[data_frame["Longitude"] < longitude_range[1]]
+    data_frame = data_frame[data_frame["Latitude"] > latitude_range[0]]
+    data_frame = data_frame[data_frame["Latitude"] < latitude_range[1]]
+
 print(data_frame.head())
 geometry = [Point(xy) for xy in zip(data_frame["Longitude"], data_frame["Latitude"])]
 geo_dataframe = gpd.GeoDataFrame(data_frame, crs={'init': "epsg:4326"}, geometry=geometry)
 
-if filter_quality:
-    print("filtering on quality..")
-    geo_dataframe = geo_dataframe[geo_dataframe["qa"] >= minimal_data_quality]
-if remove_masked_values:
-    print("removing masked values..")
-    geo_dataframe = geo_dataframe[geo_dataframe["NO"] < 1]
-if partial_plot:
-    print("removing values outside long/lat range..")
-    geo_dataframe = geo_dataframe[geo_dataframe["Longitude"] > longitude_range[0]]
-    geo_dataframe = geo_dataframe[geo_dataframe["Longitude"] < longitude_range[1]]
-    geo_dataframe = geo_dataframe[geo_dataframe["Latitude"] > latitude_range[0]]
-    geo_dataframe = geo_dataframe[geo_dataframe["Latitude"] < latitude_range[1]]
-
-print(geo_dataframe.head())
 
 fig, ax = plt.subplots(1, 1)
 geo_dataframe.plot(column="NO", ax=ax, legend=True)
