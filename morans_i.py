715bb15b0a4a1d163bb7bf4d49d945f413b2ae66
Phobos <j-elmer-prins@live.nl>
2022-12-19 16:53:54 +0100

    queen neighbors for Moran

--- morans_i.py
+++ morans_i.py
@@ -9,9 +9,16 @@ def morans_i(array: np.array):
 
     for x in range(morans_i.shape[0]):
         for y in range(morans_i.shape[1]):
-            morans_i[x, y] = (array[x, y] - avg)/m2 * sum(get_rook_neighbors((x, y), array) - avg)
+            morans_i[x, y] = (array[x, y] - avg)/m2 * sum(get_neighbors((x, y), array, queen=True) - avg)
 
     return morans_i
+
+
+def get_neighbors(index: tuple, array: np.array, queen=True):
+    neighbors = get_rook_neighbors(index, array)
+    if queen:
+        neighbors += get_queen_neighbors(index, array)
+    return neighbors
 
 
 def get_rook_neighbors(index: tuple, array: np.array):
@@ -25,5 +32,18 @@ def get_rook_neighbors(index: tuple, array: np.array):
     if index[1] + 1 < array.shape[1]:
         neighbors.append(array[index[0], index[1] + 1])
 
+    return neighbors
+
+
+def get_queen_neighbors(index: tuple, array: np.array):
+    neighbors = []
+    if index[0] - 1 >= 0 and index[1] - 1 >= 0:
+        neighbors.append(array[index[0] - 1, index[1] - 1])
+    if index[0] + 1 < array.shape[0] and index[1] - 1 >= 0:
+        neighbors.append(array[index[0] + 1, index[1] - 1])
+    if index[0] - 1 >= 0 and index[1] + 1 < array.shape[1]:
+        neighbors.append(array[index[0] - 1, index[1] + 1])
+    if index[0] + 1 < array.shape[0] and index[1] + 1 < array.shape[1]:
+        neighbors.append(array[index[0] + 1, index[1] + 1])
     return neighbors
 
